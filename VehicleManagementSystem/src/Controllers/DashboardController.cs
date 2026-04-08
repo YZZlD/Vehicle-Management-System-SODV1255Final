50f8f257b5d0c73b9ff6da1290ccf2b576e621e0
YZZlD <max.verhulst@hotmail.com>
2026-04-08 15:51:01 -0600

    feat: skeleton for DashboardController. This handles the overview information for the application and shoul dhave access to all of the repositories to pull necessary information to display overview of system state.

---
+++ VehicleManagementSystem/src/Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleManagementSystem.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly CustomerRepository _customerRepository;
+
+        private readonly VehicleRepository _vehicleRepository;
+
+        private readonly ReservationRepository _reservationRepository;
+
+        public DashboardController(CustomerRepository customerRepository, VehicleRepository vehicleRepository, ReservationRepository reservationRepository)
+        {
+            _customerRepository = customerRepository;
+            _vehicleRepository = vehicleRepository;
+            _reservationRepository = reservationRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            /*
+                REPOSITORY SHOULD BE CALLED IN INDEX TO GET THE FOLLOWING ITEMS FOR THE DASHBOARD:
+
+                    - Most recent items for each model (count of 3)
+                    - All vehicle availability information
+                    - Upcoming reservation expirations/return dates
+                    - Overdue reservation return dates
+
+                ALL NECESSARY INFORMATION WILL BE PASSED AND RENDERED IN THE DASHBOARD VIEW INDEX
+            */
+
+            return View();
+        }
+    }
+}
