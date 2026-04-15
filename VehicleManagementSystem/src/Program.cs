d189285d4ccb505802e28724c6f49db67513e538
ceins <ceinsmic@gmail.com>
2026-04-14 21:03:36 -0600

    added database stuff TODO: Repo?

--- VehicleManagementSystem/src/Program.cs
+++ VehicleManagementSystem/src/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using VehicleManagementSystem.src.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<VEHICLEMODELDB>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<USERMODELDB>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<RESERVATIONDB>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//database configured to use integrated security
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
