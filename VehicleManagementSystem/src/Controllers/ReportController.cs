abbd71e4ad8c4abb804d1b8d64c369c3d9c60438
YZZlD <max.verhulst@hotmail.com>
2026-04-16 18:10:08 -0600

    feat: report UI + report controller logic

--- VehicleManagementSystem/src/Controllers/ReportController.cs
+++ VehicleManagementSystem/src/Controllers/ReportController.cs
@@ -1,5 +1,6 @@
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace VehicleManagementSystem.Controllers
 {
@@ -14,34 +15,67 @@ namespace VehicleManagementSystem.Controllers
 
         //Filtering is handled through regetting the route with new parameter information internally from the generate button through the view.
         //With empty parameters all reservations will be included.
-        public async Task<IActionResult> Index(string dateFrom, string dateTo, string vehicles, string customers)
+        public async Task<IActionResult> Index(string dateFrom, string dateTo, string vehicleMake, int? minAge, int? maxAge)
         {
-            var reservations = _reservationRepository.GetAllReservations();
+            //THIS MAY BREAK WITH REPOSITORY INTRODUCTION SO IT WILL BE FIXED THEN
 
-            DateTime lowerboundDate = string.IsNullOrEmpty(dateFrom.Trim()) ? DateTime.Parse("01-01-2000") : DateTime.Parse(dateFrom);
-            DateTime upperboundDate = string.IsNullOrEmpty(dateTo.Trim()) ? DateTime.Today : DateTime.Parse(dateTo);
+            var reservations = await _reservationRepository.GetAllReservations().Include(reservation => reservation.Vehicle).Include(reservation => reservation.Customer);
 
-            List<string> vehicleList = vehicles.Split(',').ToList();
-            List<string> customerList = customers.Split(',').ToList();
+            var makes = await _reservationRepository
+                                .GetAllReservations()
+                                .Include(reservation => reservation.Vehicle)
+                                .Select(reservation => reservation.Vehicle.Make)
+                                .Distinct()
+                                .ToList();
+
+            DateTime from = string.IsNullOrWhiteSpace(dateFrom) ? DateTime.MinValue : DateTime.Parse(dateFrom);
+            DateTime to = string.IsNullOrWhiteSpace(dateTo) ? DateTime.MaxValue : DateTime.Parse(dateTo);
+
+            var filteredReservations = reservations.Where(reservation => reservation.ReservedDate >= from && reservation.ReservedDate <= to);
+
+            if(!string.IsNullOrWhiteSpace(vehicleMake))
+            {
+                filteredReservations = filteredReservations.Where(reservation => reservation.Vehicle.Make == vehicleMake);
+            }
+
+            if(minAge.HasValue)
+            {
+                filteredReservations = filteredReservations.Where(reservation => reservation.Customer.Age >= minAge.Value);
+            }
+
+            if(maxAge.HasValue)
+            {
+                filteredReservations = filteredReservations.Where(reservation => reservation.Customer.Age <= maxAge.Value);
+            }
 
             //Basic first filtering implementation (UNTESTED)
             //Logic error here as it will return nothing if either vehicle or customer are empty.
-            var filteredReservations = reservations
-                                        .Where(r => r.DateTime >= lowerboundDate && r.DateTime <= upperboundDate)
-                                        .Where(r => vehicleList.Contains(r.Vehicle))
-                                        .Where(r => customerList.Contains(r.Customer))
-                                        .ToList();
+            // var filteredReservations = reservations
+            //                             .Where(r => r.DateTime >= from && r.DateTime <= to)
+            //                             .Where(r => vehicleList.Contains(r.Vehicle))
+            //                             .Where(r => customerList.Contains(r.Customer))
+            //                             .ToList();
 
             //Basic logic grabs for different necessary information for the view (UNTESTED)
-            var totalRevenue = filteredReservations.Sum(r => r.revenue);
+            var totalRevenue = filteredReservations.Sum(reservation => reservation.TotalPrice);
             var totalReservations = filteredReservations.Count();
-            var activeRentals = filteredReservations.Where(r => DateTime.Today >= r.ReservedDate && DateTime.Today <= r.DueDate).ToList().Count();
-            var mostCommonVehicle = filteredReservations.GroupBy(r => r.Vehicle)
+            var activeRentals = filteredReservations.Where(reservation => DateTime.Today >= reservation.ReservedDate && DateTime.Today <= reservation.DueDate).ToList().Count();
+            var mostPopularVehicle = filteredReservations.GroupBy(reservation => reservation.Vehicle.Model)
                                         .OrderByDescending(group => group.Count)
                                         .Select(group => group.Key)
-                                        .First();
+                                        .FirstOrDefault();
 
-            return View(filteredReservations, totalRevenue, totalReservations, activeRentals, mostCommonVehicle);
+            var reportViewModel = new ReportViewModel
+            {
+                Reservations = filteredReservations,
+                TotalRevenue = totalRevenue,
+                TotalReservations = totalReservations,
+                ActiveRentals = activeRentals,
+                MostPopularVehicle = mostPopularVehicle,
+                Makes = makes
+            };
+
+            return View(reportViewModel);
         }
     }
 }
