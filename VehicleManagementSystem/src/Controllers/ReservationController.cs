3aac74c0922e20c44e5502ab4491d3f80221201b
YZZlD <max.verhulst@hotmail.com>
2026-04-16 18:56:31 -0600

    feat: reservation controller updated with proper logic and mvc flow

--- VehicleManagementSystem/src/Controllers/ReservationController.cs
+++ VehicleManagementSystem/src/Controllers/ReservationController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.VisualBasic;
 
 namespace VehicleManagementSystem.Controllers
 {
@@ -24,60 +25,80 @@ namespace VehicleManagementSystem.Controllers
             return View(reservations);
         }
 
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> SelectDate()
         {
-            ViewBag.Customers = _customerRepository.GetAllCustomers();
-            ViewBag.Vehicles = _vehicleRepository.GetAllVehicles();
-
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> SelectDate(DateTime reservedDate, DateTime dueDate)
         {
-            /*
-                NEW RESERVATION OBJECT IS CREATED AFTER AVAILABILITY VALIDATION
-            */
+            var reservedVehicleIds = await _reservationRepository.GetAllReservations()
+                                        .Where(r => r.ReservedDate < dueDate && r.DueDate > reservedDate)
+                                        .Select(r => r.VehicleId)
+                                        .Distinct()
+                                        .ToList();
+
+            var availableVehicles = await _vehicleRepository.GetAllVehicles()
+                                        .Where(vehicle => !reservedVehicleIds.Contains(vehicle.VehicleId))
+                                        .ToList();
+
+            ViewBag.Customers = await _customerRepository.GetAllCustomers();
+            ViewBag.Vehicles = availableVehicles;
+            ViewBag.ReservedDate = reservedDate;
+            ViewBag.DueDate = dueDate;
+
+            return View("Create");
         }
 
-        /*
+        [HttpPost]
+        public async Task<IActionResult> Create(int customerId, int vehicleId, DateTime reservedDate, DateTime dueDate)
+        {
+            var customer = await _customerRepository.GetCustomerById(customerId);
+            var vehicle = await _vehicleRepository.GetVehicleById(vehicleId);
 
-        LOOKING TO IMPLEMENT THIS FLOW FOR CREATION.
-            1. Pass view with date range selector
-            2. Post to controller that filters vehicle availability and passes it to a viewbag to return next form view
-            3. Form view with filtered available vehicles and all customers post
-            4. Use vehicle and customer information with dates to make a new reservation
+            var reservation = new Reservation
+            {
+                CustomerId = customerId,
+                VehicleId = vehicleId,
+                ReservedDate = reservedDate,
+                DueDate = dueDate,
+                PriceTotal = CalculatePrice(vehicle, reservedDate, dueDate)
+            };
 
-        */
+            await _reservationRepository.Add(reservation);
+            return RedirectToAction("Index");
+        }
 
         public async Task<IActionResult> Edit(int id)
         {
-            var reservation = _reservationRepository.GetReservationById(id);
+            var reservation = await _reservationRepository.GetReservationById(id);
 
-            if(!reservation) return NotFound();
+            if(reservation == null) return NotFound();
             return View(reservation);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(int id, ReservationDTO reservation)
+        public async Task<IActionResult> Edit(int id, DateTime? returnedDate)
         {
-            var reservation = new Reservation
-            {
-                /*
-                    RESERVATION OBJECT IS CREATED FROM RESERVATIONDTO FIELDS GRABBED FROM FORM SUBMISSION
-                */
-            };
+            var reservation = await _reservationRepository.GetReservationById(id);
 
-            await _reservationRepository.UpdateReservation(id, reservation);
+            if(reservation == null) return NotFound();
+
+            var vehicle = await _vehicleRepository.GetVehicleById(reservation.VehicleId);
+
+            reservation.ReturnedDate = returnedDate;
+
+            reservation.PriceTotal = CalculatePrice(vehicle, reservation.ReservedDate, reservation.DueDate, reservation.ReturnedDate);
+
+            await _reservationRepository.UpdateReservation(reservation);
+
             return RedirectToAction("Index");
         }
 
         [HttpPost]
         public async Task<IActionResult> Delete(int id)
         {
-            var reservation = await _reservationRepository.GetReservationById(id);
-            if(!reservation) return NotFound();
-
             await _reservationRepository.DeleteReservation(id);
 
             return RedirectToAction("Index");
